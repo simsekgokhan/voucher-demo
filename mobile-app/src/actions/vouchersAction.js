73d62687bfdb3465b1849cb54a67c9becbbbd250
Gokhan Simsek <g.simsek@paralect.com>
2018-03-15 12:20:14 +0300

    Removing addVoucher functionality to send message to web server

--- mobile-app/src/actions/vouchersAction.js
+++ mobile-app/src/actions/vouchersAction.js
@@ -1,19 +1,6 @@
 import moment from 'moment';
-import voucherConstants from '../common/voucher.constants'
-import { addTransaction } from '../resources/transaction/transaction.api';
 
 export function addVoucher(voucher) {
-    if (voucher.qrScanned) {
-        const email = voucherConstants.RECEIVED_EMAIL;
-        const person = email.replace(/(\w+)\.(\w+).+$/, '$1 $2');
-        addTransaction({
-            id: voucher.id,
-            amount: `${voucher.amount}`,
-            email,
-            person,
-            date: voucher.timeStamp,
-        });
-    }
     return {
         type: "ADD_VOUCHER",
         payload: voucher,
